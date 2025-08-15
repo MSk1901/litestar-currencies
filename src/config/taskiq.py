419b20bad5670792f69d479cf090684a595e07d4
Mariya Kuznetsova <kuznetsova19.m@gmail.com>
2025-08-15 18:08:07 +0300

    add task scheduler

---
+++ src/config/taskiq.py
@@ -0,0 +1,30 @@
+from taskiq.schedule_sources import LabelScheduleSource
+from taskiq_faststream import BrokerWrapper, StreamScheduler
+
+from src.apps.datasource.parser.constants import DataSourceEnum
+from src.config.faststream import faststream_broker
+
+taskiq_broker = BrokerWrapper(faststream_broker)
+
+taskiq_broker.task(
+    message=DataSourceEnum.CBR,
+    queue="fetch.cbr.rates",
+    schedule=[
+        {
+            "cron": "0 1 * * *",
+        }
+    ],
+)
+
+# taskiq_broker.task(
+#     message=DataSourceEnum.OTHER,
+#     queue="fetch.OTHER.rates",
+#     schedule=[{
+#         "cron": "5 1 * * *",
+#     }],
+# )
+
+scheduler = StreamScheduler(
+    broker=taskiq_broker,
+    sources=[LabelScheduleSource(taskiq_broker)],
+)
