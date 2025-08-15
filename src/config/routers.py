98c985bec31eeeecad7d5d56d02dfbf22487a8ef
Mariya Kuznetsova <kuznetsova19.m@gmail.com>
2025-08-15 20:59:14 +0300

    fix import

--- src/config/routers.py
+++ src/config/routers.py
@@ -1,5 +1,5 @@
 from litestar import Router
 
-from apps.currency_rate.routers import rate_router
+from src.apps.currency_rate.routers import rate_router
 
 api_router = Router(path="/api", route_handlers=[rate_router])
