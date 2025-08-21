b441fa717810148eb1cb036cf7bae3b95be5ef1a
Mariya Kuznetsova <kuznetsova19.m@gmail.com>
2025-08-21 18:27:32 +0300

    add debug param to litestar app

--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
 from litestar import Litestar
 
 from src.apps.datasource.tasks import fetch_rates_handler
-from src.config import api_router, faststream_broker, openapi_config, sqlalchemy_plugin
+from src.config import api_router, config, faststream_broker, openapi_config, sqlalchemy_plugin
 
 app = Litestar(
+    debug=config.app.DEBUG,
     plugins=[sqlalchemy_plugin],
     on_startup=[faststream_broker.start],
     on_shutdown=[faststream_broker.stop],
