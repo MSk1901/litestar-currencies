16abb0ca643cf1bf9ca8931d722b09da2d4a2df3
Mariya Kuznetsova <kuznetsova19.m@gmail.com>
2025-08-25 19:27:54 +0300

    improve get_actual_rates endpoint

--- src/apps/currency_rate/routers.py
+++ src/apps/currency_rate/routers.py
@@ -2,4 +2,4 @@ from litestar import Router
 
 from src.apps.currency_rate.controllers import CurrencyRateController
 
-rate_router = Router(path="", route_handlers=[CurrencyRateController])
+rate_router = Router(path="/rates", tags=["Курсы валют"], route_handlers=[CurrencyRateController])
